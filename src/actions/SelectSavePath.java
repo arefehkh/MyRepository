378b7dd7a2ea25c42422b7c5a45c3ac07a786387
MahshidExm <mahshid.mygit@gmail.com>
2014-07-30 11:15:48 -0700

    Update SelectSavePath.java

--- src/actions/SelectSavePath.java
+++ src/actions/SelectSavePath.java
@@ -6,6 +6,18 @@ import java.awt.event.ActionListener;
 import java.io.File;
 
 public class SelectSavePath implements ActionListener {
+   private String pathFile;
+
+  @Override
+    public void actionPerformed(ActionEvent e) {
+        JFileChooser fileChooser = new JFileChooser(pathFile);
+        fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        if (fileChooser.showSaveDialog(Storage.getMainFrame().getRootPane()) == JFileChooser.APPROVE_OPTION) {
+            file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+            Storage.setSaveFilePath(file.getAbsolutePath());
+        }
+
+    }
 
    
 }
