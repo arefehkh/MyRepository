7096c800b4427bd15a708fcca72f6455fa3bb173
MohammadExm <Mohammad.MyGit@gmail.com>
2014-07-31 00:38:44 -0700

    Create Utility.java

---
+++ src/components/Utility.java
@@ -0,0 +1,21 @@
+package components;
+
+import ir.hoobakht.Storage;
+
+import java.util.regex.Matcher;
+
+public class Utility {
+
+    public static String getParent(String path) {
+        String elementName = "";
+        String[] pp = path.split(Matcher.quoteReplacement(Storage.getFileSeparator()));
+        if (pp.length == 0) {
+            elementName = "";
+        } else if (pp.length < 2) {
+            elementName = pp[0];
+        } else if (pp.length >= 2) {
+            elementName = pp[pp.length - 2];
+        }
+        
+    }
+}
