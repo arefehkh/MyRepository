51ced5fc2518644229c8634a80bd6e66fc600913
arefehkh <arefe_khalili@yahoo.com>
2014-08-03 11:58:54 -0700

    Update Contents.java

--- src/resource/Contents.java
+++ src/resource/Contents.java
@@ -1,24 +1,14 @@
 package resource;
 
-
 public class Constants {
 
     public static String ROOT_ENAME = "XpDiag";
-  
     public static String ROOT_PIC = "0";
     public static String ROOT_INFO = "1.0.0";
     public static String ROOT_TYPE = "Pnl";
-
     public static String PANEL_INFO = "---";
     public static String PANEL_PIC = "---";
     public static String PANEL_TYPE = "Pnl";
-
-
-    //public static String ECU_INFO = "---";
-
-
     public static String ECU_TYPE = "ECU";
-
-
 
 }
